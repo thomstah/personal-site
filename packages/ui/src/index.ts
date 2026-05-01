5cb1ca897d7e30c1d081b41289091f1c0f2ba714
thomstah <thommyxay@gmail.com>
2026-04-30 20:58:20 -0500

    feat: export packages/ui barrel

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,3 @@
-export {};
+export { tokens, colors, fontSizes, spacing, animation } from './tokens';
+export { PixelCharacter } from './components/PixelCharacter';
+export { PageTransition } from './components/PageTransition';
